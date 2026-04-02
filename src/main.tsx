f62294f6b4d5c9168a10b60f6967107487eb11d8
kaio keinner <keinnerkaio@gmail.com>
2026-04-02 14:59:52 -0400

    fix: troca BrowserRouter por HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import { createRoot } from 'react-dom/client';
 import "./index.css";
 
 import App from './App.tsx'
-import { BrowserRouter } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
