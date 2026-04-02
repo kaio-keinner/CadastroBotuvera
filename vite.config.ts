f92f83742e5c85033af86435c1c99fe73fdb2a01
kaio keinner <keinnerkaio@gmail.com>
2026-04-02 14:06:23 -0400

    implementa deploy_2

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_ENV === 'gh-pages' ? '/Site_botuvera/' : '/',
+  // Ajustei para o nome do seu repositório no GitHub
+  base: '/CadastroBotuvera/', 
   plugins: [react(), tailwindcss()],
 })
